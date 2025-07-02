b65b6d8cc32b861367e0eec2f460d8b7da9297c5
ServaneBeaudier <servanebeaudier@gmail.com>
2025-07-02 16:36:39 +0200

    mise en place du footer

--- pagepals-front/src/app/app.ts
+++ pagepals-front/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
